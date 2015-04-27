1a2a1e2a088af218cf12c128d8fd119b5070a830
paulsimeon <paulsimeon@gmail.com>
2015-04-26 18:55:07 -0700

    first commit

---
+++ components/js/app/main.js
@@ -0,0 +1,9 @@
+// Declare app level module which depends on views, and components
+'use strict';
+
+(function(){
+	var app = angular.module('expPhys',[]);
+	app.controller('moduleCtrl',['$scope', function($scope){
+
+	}]);
+})();
